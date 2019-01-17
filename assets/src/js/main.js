4e651f998ccc7b7511cb3fa64705569def090c3b
MaksimLavrenyuk <maksim.lavrenyuk@outlook.com>
2019-01-17 16:21:02 +0400

    Add NPM packages

---
+++ assets/src/js/main.js
@@ -0,0 +1,28 @@
+//ПЛАГИНЫ
+
+// Импортируем jQuery
+//= ../../../node_modules/jquery/dist/jquery.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+
+//= ../../../node_modules/bootstrap/js/dist/util.js
+
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+
+
+// Импортируем Fancybox.js
+//= ../../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
+
+// Импортируем slick-carousel
+//= ../../../node_modules/slick-carousel/slick/slick.min.js
+
+// Импортируем owl-carousel.js
+//= ../../../node_modules/owl.carousel/dist/owl.carousel.min.js
+
+// Импортируем wow
+//= ../../../node_modules/wow.js/dist/wow.min.js
+
+// Импортируем другие js-файлы
+
+//файл с самописными скриптами
+//= my.js
